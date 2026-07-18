68b993e397db1c20689c8a6ab8df7ab811c1f9d9
kevlogg <loggia.1996@gmail.com>
2026-07-18 00:52:14 -0300

    fix: agregar lupa.mp4 al passthrough copy de 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("icono");
   eleventyConfig.addPassthroughCopy("lottie");
   eleventyConfig.addPassthroughCopy("planta.png");
+  eleventyConfig.addPassthroughCopy("lupa.mp4");
 
   return {
     dir: {
